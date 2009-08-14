56426240bdf26d2ff44dde4deecf5cb15e4201f4
Adam Benzan <adam.benzan@gmail.com>
2009-08-14 15:27:13 -0700

    fix initial map zoom/center, add space for toggler

--- goodplaces/context.py
+++ goodplaces/context.py
@@ -3,10 +3,10 @@ from django.db.models import Avg, Max, Min
 
 def map_vars(request):
     places = Place.objects.all()
-    avgs = places.aggregate(Avg('lng'),Avg('lat'),Max('lng'),Max('lat'),Min('lng'), Min('lat'))
     cloud = Place.tag_manager.cloud()
     if request.GET.get('tag'):
         places = Place.tagged_item_manager.with_any([request.GET.get('tag'),])
+    avgs = places.aggregate(Avg('lng'),Avg('lat'),Max('lng'),Max('lat'),Min('lng'), Min('lat'))
     return {'places': places,
             'cloud' : cloud,
             'avg_lng' : avgs['lng__avg'],
