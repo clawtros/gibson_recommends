3bfd728dab99a979e16f59b44fb3ee53c9ed038e
Adam Benzan <adam.benzan@gmail.com>
2009-08-14 14:10:45 -0700

    in this version it implements the changes requested

--- goodplaces/context.py
+++ goodplaces/context.py
@@ -1,13 +1,18 @@
 from models import Place
-from django.db.models import Avg
+from django.db.models import Avg, Max, Min
 
 def map_vars(request):
     places = Place.objects.all()
-    avgs = places.aggregate(Avg('lng'),Avg('lat'))
+    avgs = places.aggregate(Avg('lng'),Avg('lat'),Max('lng'),Max('lat'),Min('lng'), Min('lat'))
     cloud = Place.tag_manager.cloud()
     if request.GET.get('tag'):
         places = Place.tagged_item_manager.with_any([request.GET.get('tag'),])
     return {'places': places,
             'cloud' : cloud,
             'avg_lng' : avgs['lng__avg'],
-            'avg_lat' : avgs['lat__avg']}
+            'avg_lat' : avgs['lat__avg'],
+            'min_lng' : avgs['lng__min'],
+            'min_lat' : avgs['lat__min'],
+            'max_lng' : avgs['lng__max'],
+            'max_lat' : avgs['lat__max'],
+            }
